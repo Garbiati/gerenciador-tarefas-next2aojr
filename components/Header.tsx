b1d0e7d5dc9201ffe1f59170222df4bc38c74f46
Garbiati <a.garbiati@gmail.com>
2022-12-20 13:57:23 -0300

    Atualização da aula final

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,23 @@ import { NextPage } from "next";
 
 type HeaderProps = {
     sair():void
+    togglModal():void
 }
 
-export const Header: NextPage<HeaderProps> = ({sair}) => {
+export const Header: NextPage<HeaderProps> = ({sair, togglModal}) => {
+    const fullName = localStorage.getItem('name');
+    const firstName = fullName?.split(' ')[0] || '';
+
     return (
         <div className="container-header">
             <img src="/logo.svg" alt="Logo Fiap" className="logo"/>
-            <button><span>+</span>Adicionar tarefa</button>
+            <button onClick={togglModal}><span>+</span>Adicionar tarefa</button>
             <div className="desktop">
-                <span>Olá, ...</span>
+                <span>Olá, {firstName}</span>
                 <img src="/exit-desktop.svg" alt="Sair" onClick={sair}/>
             </div>
             <div className="mobile">
-                <span>Olá, ...</span>
+                <span>Olá, {firstName}</span>
                 <img src="/exit-mobile.svg" alt="Sair" onClick={sair}/>
             </div>
         </div>
